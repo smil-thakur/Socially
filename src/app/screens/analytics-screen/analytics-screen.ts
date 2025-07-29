bf11ee5227f72ee3db111ee82a3e498cb85f23d5
smil-thakur <smil.raj.thakur@gmail.com>
2025-07-29 10:13:25 +0530

    WIP analytics page, implement accordion for better UX

--- src/app/screens/analytics-screen/analytics-screen.ts
+++ src/app/screens/analytics-screen/analytics-screen.ts
@@ -18,6 +18,17 @@ import { HlmInputDirective } from '@spartan-ng/helm/input';
 import { BasePageScreen } from '../../common/base-page-screen/base-page-screen';
 import { UserService } from '../../services/user-service';
 import { HlmButtonDirective } from '@spartan-ng/helm/button';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  HlmAccordionContentComponent,
+  HlmAccordionDirective,
+  HlmAccordionIconDirective,
+  HlmAccordionItemDirective,
+  HlmAccordionTriggerDirective,
+} from '@spartan-ng/helm/accordion';
+import { HlmIconDirective } from '@spartan-ng/helm/icon';
+import { lucideChevronDown, lucideInfo } from '@ng-icons/lucide';
+import { HlmTooltipTriggerDirective } from '@spartan-ng/helm/tooltip';
 
 @Component({
   selector: 'app-analytics-screen',
@@ -33,9 +44,23 @@ import { HlmButtonDirective } from '@spartan-ng/helm/button';
     OnlyBackNavBar,
     HlmButtonDirective,
     HlmCardFooterDirective,
+    HlmAccordionItemDirective,
+    HlmAccordionTriggerDirective,
+    HlmAccordionIconDirective,
+    HlmAccordionContentComponent,
+    HlmAccordionDirective,
+    NgIcon,
+    HlmIconDirective,
+    HlmTooltipTriggerDirective,
   ],
   templateUrl: './analytics-screen.html',
   styleUrl: './analytics-screen.scss',
+  providers: [
+    provideIcons({
+      lucideChevronDown,
+      lucideInfo,
+    }),
+  ],
 })
 export class AnalyticsScreen extends BasePageScreen {
   private userService = inject(UserService);
